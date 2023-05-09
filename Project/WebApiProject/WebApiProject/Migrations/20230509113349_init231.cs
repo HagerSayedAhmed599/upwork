090cd7655dab37eacd9636952beab94cf04d0a2c
hager_sayed <hager.sayed226778@gmail.com>
2023-05-09 21:02:15 +0300

    project

---
+++ Project/WebApiProject/WebApiProject/Migrations/20230509113349_init231.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class init231 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Client",
+                table: "profile",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Freelancer",
+                table: "profile",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Client",
+                table: "profile");
+
+            migrationBuilder.DropColumn(
+                name: "Freelancer",
+                table: "profile");
+        }
+    }
+}
